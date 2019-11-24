161189a9f8916dceecddbf92af2043f67b1228f1
asd412id <asdar.binsyam@gmail.com>
2019-11-24 08:36:03 +0800

    Change multi fcgi port to spawn child

--- start.bat
+++ start.bat
@@ -1,8 +1,12 @@
 @echo off
 
-SET PHP_FCGI_MAX_REQUESTS=1000
-for /L %%i in (0,1,%1) do (
-	@start /b %~dp0\php\php-cgi.exe -b 127.0.0.1:900%%i -c %~dp0\php\php.ini
+SET child=%1
+IF "%i"=="" (
+	SET child=5
 )
+
+SET PHP_FCGI_CHILDREN=%child%
+SET PHP_FCGI_MAX_REQUESTS=0
+@start /b %~dp0\php\php-cgi.exe -b 127.0.0.1:9000 -c %~dp0\php\php.ini
 
 @start /b %~dp0\start-nginx.exe -g "daemon off;" -p .
